e98915bdc93c4b529b8e6baaafe337cb11c02748
tip <cirzxs@gmail.com>
2023-12-08 11:58:43 +0700

    done day1

--- day1.py
+++ day1.py
@@ -1,2 +1,16 @@
-import requests
-requests.get
+data = open('day1.txt', 'r').read().split('\n')
+
+res = 0
+
+for line in data:
+    first_digit = ""
+    last_digit = line[0]
+    for c in line:
+        if c.isdigit():
+            if first_digit == "":
+                first_digit = c
+            last_digit = c
+    string_number = f"{first_digit}{last_digit}"
+    res += int(string_number)
+
+print(res)
